f53d7e81805538744a0366bd24c1eda7133d5eb6
Joao Felipe <joao.felipe.camargo.jfc@gmail.com>
2025-05-10 18:52:12 -0300

    add footer

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,18 +1,15 @@
-const carrosel = document.querySelector('.carrosel-over')
+const elementos = document.querySelector('.carrosel-over')
 const prevBnt = document.querySelector ('.seta-left')
 const nextBnt = document.querySelector ('.seta-right')
 
-prevBnt.addEventListener('click', function () {
+let pixels = 0
 
-    carrosel.style.transform = 'translateX(-100px)';
+prevBnt.addEventListener('click', function(){
+  pixels -= 20
+  elementos.style = `transform: translateX(${pixels}%)`
+})
 
-});
-
-nextBnt.addEventListener('click', function () {
-
-    carrosel.style.transform = 'translateX(300px)';
-    carrosel.style.transition = '2s ease in out'
-
-
-
-});
+nextBnt.addEventListener('click', function(){
+  pixels += 20
+  elementos.style = `transform: translateX(${pixels}%)`
+})
